8f7c7da0fea38d6389c933c38b3c0f0457d1322f
ni <nichenevercosa@gmail.com>
2020-07-26 22:07:03 -0300

    local

--- src/Apollo/client.js
+++ src/Apollo/client.js
@@ -1,6 +1,6 @@
 import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost';
 
 export const client = new ApolloClient({
-  link: new HttpLink({uri: 'http://192.168.15.14:3000/graphql'}),
+  link: new HttpLink({uri: 'http://localhost:3000/graphql'}),
   cache: new InMemoryCache(),
 });
