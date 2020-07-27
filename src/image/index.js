ae63463c95d288511ddde157a739b11eb041f59c
ni <nichenevercosa@gmail.com>
2020-07-26 22:00:24 -0300

    icons

---
+++ src/image/index.js
@@ -0,0 +1,14 @@
+export const LOGO = require('./ic_launcher.png');
+export const ACCOUNT_ICON = require('./account_icon.png');
+export const PERSON = require('./person.png');
+export const ADD = require('./add.png');
+export const ADD_BLACK = require('./add_black.png');
+
+//exercises
+export const EX1 = require('./ex1.png');
+export const EX2 = require('./ex2.png');
+export const EX3 = require('./ex3.png');
+export const EX4 = require('./ex4.png');
+export const EX5 = require('./ex5.png');
+export const EX6 = require('./ex6.png');
+export const EX7 = require('./ex7.png');
